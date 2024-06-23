da1f08d3cdc76e2ec19deae29cbdd2b0ae19f924
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-06-24 02:57:30 +0800

    [fix] add copyright in footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<footer class="bg-white w-full h-fit flex items-center border border-t-zinc-200">
+<footer class="bg-white w-full h-fit flex flex-col items-center border border-t-zinc-200 cursor-default">
   <div class="w-9/12 mx-auto py-5 flex flex-row justify-between items-center">
     <a href="/">
       <img
@@ -14,6 +14,7 @@
     <div class="text-sm font-medium text-end">
       <p>Dibuat oleh <a href="https://habibi2004.pages.dev" class="underline hover:text-violet-500">Ahmad Habibi</a></p>
       <p>Petanyaan dan Feedback? Telegram: <a href="https://t.me/ahmadhabibi14" class="underline hover:text-violet-500">ahmadhabibi14</a></p>
+      <p class="text-zinc-500 text-xs mt-3">Copyright © 2024.</p>
     </div>
   </div>
 </footer>
